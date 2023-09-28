d056f6bb8da082e9de68263a2778e28abc34c303
BIGJAY5000 <ocampo.616058@swl.k12.oh.us>
2023-09-28 09:23:00 -0400

    age

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int age;
+        System.out.print("Please enter your age here:");
+        age = scan.nextInt();
+        if (age >= 21) {
+            System.out.println("You get a paper wristband!");
+        }
+        else if(age < 0) {
+            System.out.println("You have entered an invalid age");
+            System.exit(0);
+        }
+        else {
+            System.out.println("You do not receive a paper wristband!");
+        }
+
     }
 }
